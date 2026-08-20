15827adb0ce34c8905aad8759582993fd7dedeb3
jianyangdu3-ux <jianyangdu3@gmail.com>
2026-08-20 15:55:44 +0800

    V10：全词库剧情覆盖 5496/5496

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='fengyin-v9-0';
+const CACHE_NAME='fengyin-v10-0';
 const CORE=[
   './','./index.html','./vocab.js','./manual.html','./manifest.webmanifest',
   './assets/app-icon.svg','./assets/char-maid.webp','./assets/char-steward.webp',
