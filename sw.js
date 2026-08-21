a9bed8e8a2b8ee4d8acbf3b51d9f7b0df5ce6d34
jianyangdu3-ux <jianyangdu3@gmail.com>
2026-08-21 19:21:21 +0800

    V11.5：剧情隐藏词义并修复后期红词点击

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='fengyin-v11-4';
+const CACHE_NAME='fengyin-v11-5';
 const CORE=[
   './','./index.html','./vocab.js','./manual.html','./manifest.webmanifest',
   './assets/app-icon.svg','./assets/char-maid.webp','./assets/char-steward.webp',
